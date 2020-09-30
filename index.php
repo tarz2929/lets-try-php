cf4d6897f44bd9936d4d21f7ea717b6da44d9cc4
tarz2929 <saitarunmulley@gmail.com>
2020-09-30 13:19:02 -0600

    started a basic webpage in localhost

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic PHP Webpage</title>
+
+    <!--Style(s)  -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <!-- Script(s) -->
+    <script type="text/JavaScript" src="js/scripts.js" defer></script>
+
+</head>
+<body>
+    <h1> hello </h1>
+    
+    
+</body>
+</html>
