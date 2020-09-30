d1731b227564c111ae10824882df3dc88f6bfd7e
tarz2929 <saitarunmulley@gmail.com>
2020-09-30 13:25:57 -0600

    add assignment

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-<!DOCTYPE html>
+<?php
+  // Assigning a variable.
+  $pageTitle = 'TECHCareers | Basic PHP Webpage';
+?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic PHP Webpage</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $pageTitle; // We can echo it, or change it, wherever we need to! ?></title>
 
-    <!--Style(s)  -->
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+  <!-- Style(s) -->
+  <link rel="stylesheet" type="text/css" href="css/main.css">
 
-    <!-- Script(s) -->
-    <script type="text/JavaScript" src="js/scripts.js" defer></script>
-
+  <!-- Script(s) -->
+  <script type="text/JavaScript" src="js/scripts.js" defer></script>
 </head>
 <body>
-    <h1> hello </h1>
-    
-    
+  <h1><?php echo $pageTitle; ?></h1>
 </body>
 </html>
